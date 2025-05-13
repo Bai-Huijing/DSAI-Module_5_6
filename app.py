72315701a5f7269795c057d4953c78759d250b5c
Bai-Huijing <huijing.karine@gmail.com>
2025-05-13 12:15:56 +0000

    updated app

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
 #gemini
 
 from flask import Flask,request,render_template
+
+app = Flask(__name__) #contract in the program to point to you as creator
+
+@app.route("/", methods = ["GET","POST"]) #decorator
+def index():
+    return(render_template("index.html")) #first page
+
+if __name__=="__main__":
+    app.run()
