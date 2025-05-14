66dbdaf16c35e1338017927add91008928829f8f
Bai-Huijing <huijing.karine@gmail.com>
2025-05-14 11:52:41 +0000

    updated notes

--- app.py
+++ app.py
@@ -4,17 +4,22 @@ from flask import Flask,request,render_template
 import google.generativeai as genai
 import os
 
+#Configure Gemini
 gemini_api_key = os.getenv("gemini_api_key")
-
 genai.configure(api_key = gemini_api_key)
+
+#Configure Gemini Model
 model = genai.GenerativeModel("gemini-2.0-flash")
 
-app = Flask(__name__) #contract in the program to point to you as creator
+#Configure Flask - contract in the program to point to you as creator
+app = Flask(__name__)
 
+#Flask
 @app.route("/", methods = ["GET","POST"]) #decorator
 def index():
     return(render_template("index1.html")) #first page
 
+#Gemini
 @app.route("/gemini", methods = ["GET","POST"]) 
 def gemini():
     return(render_template("gemini2.html")) 
